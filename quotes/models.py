842404e02b3788fb020497e5eee4480d2cf0f9bc
Natnael Mesfin <natnaelmb1@gmail.com>
2024-06-28 17:40:46 +0300

    initial models

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# animes table
+class Anime(models.Model):
+    name = models.CharField(max_length=200)
+    pic_url = models.CharField(max_length=500)
+
+# characters table
+class Character(models.Model):
+    name = models.CharField(max_length=100)
+    pic_url = models.CharField(max_length=500)
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
+
+# quotes table
+class Quote(models.Model):
+    text = models.CharField(max_length=500)
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
