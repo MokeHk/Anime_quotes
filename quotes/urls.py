64c328ccf28e35a8ae9b2c607bbc256c663fa266
Natnael Mesfin <natnaelmb1@gmail.com>
2024-06-28 19:39:18 +0300

    updated paths

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index_anime, name="index all anime"),
-    path("<int:anime_id>/", views.index_anime_characters, name="index all characters"),
-    path("<int:anime_id>/<int:character_id>/", views.index_character_quotes, name="index all quotes"),
-    path("<int:anime_id>/<int:character_id>/<int:quote_id>/", views.get_quote, name="get one quote")
+    path("<int:anime_id>/", views.index_anime_characters,
+         name="index all characters"),
+    path("character/<int:character_id>/",
+         views.index_character_quotes, name="index all quotes"),
+    path("quote/<int:quote_id>/",
+         views.get_quote, name="get one quote")
 ]
