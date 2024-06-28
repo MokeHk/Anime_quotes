5952fd0d5d169e2b3f1bc5b3b5038b437fd312e4
Natnael Mesfin <natnaelmb1@gmail.com>
2024-06-28 17:59:43 +0300

    added __str__ method

--- quotes/models.py
+++ quotes/models.py
@@ -6,13 +6,22 @@ class Anime(models.Model):
     name = models.CharField(max_length=200)
     pic_url = models.CharField(max_length=500)
 
+    def __str__(self) -> str:
+        return self.name
+
 # characters table
 class Character(models.Model):
     name = models.CharField(max_length=100)
     pic_url = models.CharField(max_length=500)
     anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return self.name + " from " + self.anime.name
+
 # quotes table
 class Quote(models.Model):
     text = models.CharField(max_length=500)
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.character.name + ": " + self.text
