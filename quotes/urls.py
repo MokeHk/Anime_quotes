b767123ea13ac4fd658e63a3b3614a29598294a2
Natnael Mesfin <natnaelmb1@gmail.com>
2024-06-28 18:30:13 +0300

    added crude paths

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index_anime, name="index all anime"),
+    path("<int:anime_id>/", views.index_anime_characters, name="index all characters"),
+    path("<int:anime_id>/<int:character_id>/", views.index_character_quotes, name="index all quotes"),
+    path("<int:anime_id>/<int:character_id>/<int:quote_id>/", views.get_quote, name="get one quote")
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Quotes Index")
+def index_anime(request):
+    return HttpResponse("Here lies all anime")
+
+def index_anime_characters(request, anime_id):
+    return HttpResponse("Here lies all character in this anime")
+
+def index_character_quotes(request, anime_id, character_id):
+    return HttpResponse("Here lies all quotes from this character")
+
+def get_quote(request, anime_id, character_id, quote_id):
+    return HttpResponse("Here lies a quote")
