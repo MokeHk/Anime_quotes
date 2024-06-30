1a4c01aad49fb036ed5641d004629964a498e2a3
Yohannes <jhon.klaus2008@gmail.com>
2024-06-30 18:07:23 +0300

    login, register and logout

--- quotes/urls.py
+++ quotes/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('register/', views.register_user, name='register'),
+    path('logout/', views.logout_user, name='logout'),
     path("", views.index_anime, name="home"),
     path("<int:anime_id>/", views.index_anime_characters,
          name="index all characters"),
