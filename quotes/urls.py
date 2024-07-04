c2e56ff3188a5953854b5e5a1e611e3862dc4965
Natnael Mesfin <natnaelmb1@gmail.com>
2024-07-04 17:33:44 +0300

    updated url names

--- quotes/urls.py
+++ quotes/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path("", views.index_anime, name="home"),
     path("<int:anime_id>/", views.index_anime_characters,
-         name="index all characters"),
+         name="index_characters"),
     path("character/<int:character_id>/",
-         views.index_character_quotes, name="index all quotes"),
+         views.index_character_quotes, name="index_quotes"),
     path("quote/<int:quote_id>/",
          views.get_quote, name="get one quote"),
-     
+    path("favorite/<int:quote_id>/", views.favorite, name="favorite_quote")
 ]
