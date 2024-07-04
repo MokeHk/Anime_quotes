61381fe94748ffee6f8ff43fc0c61c88c05a3196
Natnael Mesfin <natnaelmb1@gmail.com>
2024-07-04 14:38:03 +0300

    created user and favorite tables

--- quotes/models.py
+++ quotes/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 # animes table
+
+
 class Anime(models.Model):
     name = models.CharField(max_length=200)
     pic_url = models.CharField(max_length=500)
@@ -10,6 +12,8 @@ class Anime(models.Model):
         return self.name
 
 # characters table
+
+
 class Character(models.Model):
     name = models.CharField(max_length=100)
     pic_url = models.CharField(max_length=500)
@@ -19,9 +23,26 @@ class Character(models.Model):
         return self.name
 
 # quotes table
+
+
 class Quote(models.Model):
     text = models.CharField(max_length=500)
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.text
+
+
+# users table
+class User(models.Model):
+    user_name = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.CharField(max_length=80)
+    password = models.CharField(max_length=500)
+
+
+# favorites table
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quote = models.ForeignKey(Quote, on_delete=models.CASCADE)
